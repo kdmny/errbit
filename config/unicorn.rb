0cc99e1ecb0a194532d3026872c12407e522c4d1
Kale McNaney <kdmny30@gmail.com>
2014-09-17 20:12:49 -0700

    listen on port 8080

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,7 @@ timeout 30
 working_directory "/var/www/apps/errbit/current"
 
 listen "/tmp/web_server.sock", :backlog => 64
+listen "127.0.0.1:8080", :tcp_nopush => true
 
 pid '/tmp/web_server.pid'
 
