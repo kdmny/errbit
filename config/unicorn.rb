fae6d55cd25bde1752fceee60e330009e6bb82da
Kale McNaney <kdmny30@gmail.com>
2014-09-17 20:03:00 -0700

    unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,29 +1,36 @@
-# http://michaelvanrooijen.com/articles/2011/06/01-more-concurrency-on-a-single-heroku-dyno-with-the-new-celadon-cedar-stack/
+worker_processes 3
+timeout 30
 
-worker_processes 3 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
+working_directory "/var/www/apps/errbit"
+
+listen "/tmp/web_server.sock", :backlog => 64
+
+pid '/tmp/web_server.pid'
+
+stderr_path "/var/www/apps/errbit/shared/log/unicorn.stderr.log"
+stdout_path "/var/www/apps/errbit/shared/log/unicorn.stdout.log"
+
 preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+	GC.copy_on_write_friendly = true
 
-# Taken from github: https://github.com/blog/517-unicorn
-# Though everyone uses pretty miuch the same code
+check_client_connection false
+
 before_fork do |server, worker|
-  ##
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
- 
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
+	old_pid = '/tmp/web_server.pid.oldbin'
+	if File.exists?(old_pid) && server.pid != old_pid
+		begin
+			Process.kill("QUIT", File.read(old_pid).to_i)
+		rescue Errno::ENOENT, Errno::ESRCH
+			# someone else did our job for us
+		end
+	end
+
+	defined?(ActiveRecord::Base) and
+		ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+	defined?(ActiveRecord::Base) and
+		ActiveRecord::Base.establish_connection
 end
